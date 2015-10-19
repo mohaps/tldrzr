e2f09457f3174d3e258870f676791c13b5d26a74
Saurav Mohapatra <mohaps@gmail.com>
2015-10-19 14:21:38 -0400

    fixed typo in Word compare
    Fixed as reported in https://github.com/mohaps/xtractor/commit/12fc39490eb1c88a2ed77aaf3a6852864f56b2c0

--- src/main/java/com/mohaps/tldr/utils/Words.java
+++ src/main/java/com/mohaps/tldr/utils/Words.java
@@ -154,7 +154,7 @@ public final class Words {
 					for (int i = 0; i < s1.length() && i < s2.length(); i++) {
 						if (s1.charAt(i) > s2.charAt(i)) {
 							return -1;
-						} else if (s1.charAt(i) > s2.charAt(i)) {
+						} else if (s1.charAt(i) < s2.charAt(i)) {
 							return 1;
 						}
 					}
